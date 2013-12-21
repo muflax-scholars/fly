4264679dac2b042cd8920f9489176e5047f0d152
muflax <mail@muflax.com>
2013-12-21 19:18:54 +0000

    minor cleanup

--- models/wordchooser/spaced.py
+++ models/wordchooser/spaced.py
@@ -2,20 +2,19 @@
 
 """Retrieval of words based on spaced repetition."""
 
+from fly.models.wordchooser import interface
+
 from collections import OrderedDict
 import random
 
-from fly.models.wordchooser import interface
-
-
 class RetrieveSpaced(interface.WordChooserInterface):
 
-    """Words are retrieved randomly from plover's dict."""
+    """Words are shown in-order and then spaced out."""
 
     def __init__(self, word_translation_dict, word_list):
 
         """
-        @param word_translation_dict: dict mapping steno chord to 
+        @param word_translation_dict: dict mapping steno chord to
         english translation for every
         chord in word_list
         @param word_list: list of steno words to present to user
@@ -29,7 +28,7 @@ class RetrieveSpaced(interface.WordChooserInterface):
         self.initial_spacing    = 1.0
         self.spacing_factor     = 2.0
         self.max_spacing        = self.spacing_factor**5
-        
+
         # put every (unique) word in the new word queue and store a spacing interval for them
         self.new_queue = list(OrderedDict.fromkeys(word_list))
         self.spacing   = {word: self.initial_spacing for word in word_translation_dict.keys()}
@@ -38,7 +37,7 @@ class RetrieveSpaced(interface.WordChooserInterface):
         self.queue     = []
         self.last_word = None
         self.open_word = False
-        
+
         self.word_translation_dict = word_translation_dict
 
     def get_word_and_translation(self):
@@ -50,21 +49,21 @@ class RetrieveSpaced(interface.WordChooserInterface):
         # remove word from learning queue
         if self.queue:
             word = self.queue.pop(0)
-            
+
             # try not to repeat words
             if word == self.last_word and self.queue:
                 word = self.queue.pop(0)
                 self.queue.insert(0, self.last_word)
-            
+
             # remember that we still have an unanswered word
             self.last_word = word
             self.open_word = True
         else:
             word = self.last_word
             self.open_word = False
-            
+
         translation = self.word_translation_dict[word]
-        
+
         return word, translation
 
     def on_right_word_entered(self):
@@ -81,7 +80,7 @@ class RetrieveSpaced(interface.WordChooserInterface):
 
     def on_wrong_word_entered(self):
         word = self.last_word
-        
+
         # reset spacing and reinsert word into queue
         if self.open_word == True:
             spacing = self.initial_spacing
@@ -91,4 +90,3 @@ class RetrieveSpaced(interface.WordChooserInterface):
 
     def is_done(self):
         return not (self.open_word or self.queue or self.new_queue)
-        
